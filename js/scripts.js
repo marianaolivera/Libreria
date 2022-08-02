71ceff18b1615d2f1152215a67ccdbadb2b13c13
Mariana <marianaolivera83@gmail.com>
2022-08-02 20:24:35 -0300

    agregando fetch

--- js/scripts.js
+++ js/scripts.js
@@ -127,6 +127,28 @@ btnMostrarAlert.onclick= mostarAlert
         timer: 1500
       })
  }
+const $form = document.querySelector('#form')
+
+$form.addEventListener('submit',handleSubmit)
+
+
+async function handleSubmit(event){
+  event.preventDefault()
+  const form =new FormData(this)
+  const response =await fetch (this.action, {
+   method: this.method,
+   body:form,
+   headers:{
+    'Accept':'application/json'
+   }
+  })
+  if(response.ok){
+    this.reset()
+    alert('Gracias por contactarnos,le escribiremos pronto')
+  }
+}
+
+
 
 
 function addLocalStorage(){
